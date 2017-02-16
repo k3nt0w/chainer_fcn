9a5b2a8245aa0952c45a29ffc5281ea1d17916ba
Kento Watanabe <k3nt0.watanabe@gmail.com>
2017-02-17 00:57:25 +0900

    edit

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ from chainer import serializers
 from chainer import Variable
 
 class FCN(chainer.Chain):
-    def __init__(self, n_class=21, gpu_flag=False, pretrained=False):
+    def __init__(self, n_class=21):
         super(FCN, self).__init__(
             conv1_1=L.Convolution2D(3, 64, 3, stride=1, pad=1),
             conv1_2=L.Convolution2D(64, 64, 3, stride=1, pad=1),
@@ -68,40 +68,3 @@ class FCN(chainer.Chain):
         else:
             pred = F.softmax(h)
             return pred
-
-
-class VGG(chainer.Chain):
-    def __init__(self):
-        super(VGG, self).__init__(
-            conv1_1=L.Convolution2D(3, 64, 3, stride=1, pad=1),
-            conv1_2=L.Convolution2D(64, 64, 3, stride=1, pad=1),
-
-            conv2_1=L.Convolution2D(64, 128, 3, stride=1, pad=1),
-            conv2_2=L.Convolution2D(128, 128, 3, stride=1, pad=1),
-
-            conv3_1=L.Convolution2D(128, 256, 3, stride=1, pad=1),
-            conv3_2=L.Convolution2D(256, 256, 3, stride=1, pad=1),
-            conv3_3=L.Convolution2D(256, 256, 3, stride=1, pad=1),
-
-            conv4_1=L.Convolution2D(256, 512, 3, stride=1, pad=1),
-            conv4_2=L.Convolution2D(512, 512, 3, stride=1, pad=1),
-            conv4_3=L.Convolution2D(512, 512, 3, stride=1, pad=1),
-
-            conv5_1=L.Convolution2D(512, 512, 3, stride=1, pad=1),
-            conv5_2=L.Convolution2D(512, 512, 3, stride=1, pad=1),
-            conv5_3=L.Convolution2D(512, 512, 3, stride=1, pad=1)
-        )
-        self.train = False
-
-    def __call__(self, x):
-        h = F.relu(self.conv1_2(F.relu(self.conv1_1(x))))
-        h = F.max_pooling_2d(h, 2, stride=2)
-        h = F.relu(self.conv2_2(F.relu(self.conv2_1(h))))
-        h = F.max_pooling_2d(h, 2, stride=2)
-        h = F.relu(self.conv3_3(F.relu(self.conv3_2(F.relu(self.conv3_1(h))))))
-        p3 = F.max_pooling_2d(h, 2, stride=2)
-        h = F.relu(self.conv4_3(F.relu(self.conv4_2(F.relu(self.conv4_1(p3))))))
-        p4 = F.max_pooling_2d(h, 2, stride=2)
-        h = F.relu(self.conv5_3(F.relu(self.conv5_2(F.relu(self.conv5_1(p4))))))
-        p5 = F.max_pooling_2d(h, 2, stride=2)
-        return [p3, p4, p5]
